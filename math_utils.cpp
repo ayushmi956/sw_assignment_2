ff35cdd3789b6b37bfe2bdc67e6e72b2e1b1bc52
ayushmi956 <pavani13846@gmail.com>
2026-02-03 09:05:21 +0530

    Add files via upload

---
+++ math_utils.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file math_utils.cpp
+ * @brief Implementation of mathematical functions
+ */
+
+#include "math_utils.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
